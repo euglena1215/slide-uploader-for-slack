74121983988495f25b2247ff57ee0114637ed0fd
Shintani Teppei <teppest1215@gmail.com>
2018-11-26 03:28:44 +0900

    fix

--- src/utils/getUserLists.ts
+++ src/utils/getUserLists.ts
@@ -4,10 +4,10 @@ import { User } from "./getUserListFromSlack";
 export const getUserLists = () => {
   return USERS_SHEET.getRange(2, 1, USERS_SHEET.getLastRow() - 1, 5)
     .getValues()
-    .map(arrayToPdfFile);
+    .map(arrayToUser);
 };
 
-const arrayToPdfFile = (arr): User => {
+const arrayToUser = (arr): User => {
   return {
     userId: arr[0],
     name: arr[1],
